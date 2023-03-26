59f657a2c8dcaa843b78486bb472a4e690ac8884
Evgenii <shestakovevgeniy11@gmail.com>
2023-03-26 13:52:18 +0300

    add screenshot

--- src/entities/Comment/ui/CommentList/CommentList.stories.tsx
+++ src/entities/Comment/ui/CommentList/CommentList.stories.tsx
@@ -2,16 +2,14 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { CommentList } from './CommentList';
 
 export default {
-   title: 'shared/CommentList',
-   component: CommentList,
-   argTypes: {
-      backgroundColor: { control: 'color' },
-   },
+    title: 'shared/CommentList',
+    component: CommentList,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
 } as ComponentMeta<typeof CommentList>;
 
-const Template: ComponentStory<typeof CommentList> = (args) => <CommentList { ...args } />;
+const Template: ComponentStory<typeof CommentList> = (args) => <CommentList {...args} />;
 
 export const Normal = Template.bind({});
-Normal.args = {
-
-};
+Normal.args = {};
