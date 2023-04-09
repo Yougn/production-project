f855fdd5e12f0b5cbfad9886ec593b4b0db38275
Evgenii <shestakovevgeniy11@gmail.com>
2023-04-09 14:03:15 +0300

    production

--- src/feachers/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
+++ src/feachers/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
@@ -1,7 +1,4 @@
 import { lazy, FC } from 'react';
 import { AddCommentFormProps } from './AddCommentForm';
 
-export const AddCommentFormmAsync = lazy<FC<AddCommentFormProps>>(() => new Promise((resolve) => {
-    // @ts-ignore
-    setTimeout(() => resolve(import('./AddCommentForm')), 1500);
-}));
+export const AddCommentFormmAsync = lazy<FC<AddCommentFormProps>>(() => import('./AddCommentForm'));
