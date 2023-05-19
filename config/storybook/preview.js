4a2731b81239d074ae118a287cc4da1ad16eb8ae
Evgenii <shestakovevgeniy11@gmail.com>
2023-05-19 08:50:52 +0300

    fix unit test with aliases, fix storybook after refactoring, add addon for storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -3,7 +3,7 @@ import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-import { Theme } from '../../src/app/providers/ThemeProvider';
+import { Theme } from '../../src/shared/const/theme';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -12,6 +12,15 @@ export const parameters = {
             color: /(background|color)$/i,
             date: /Date$/,
         },
+    },
+    layout: 'fullscreen',
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#e8e8ea' },
+            { name: 'dark', class: Theme.DARK, color: '#090949' },
+            { name: 'orange', class: Theme.ORANGE, color: '#bd5012' },
+        ],
     },
 };
 
